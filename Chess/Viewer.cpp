374586ad2a581a161899ffa9277203ba14245515
CA-Lee <email@calee.tw>
2021-05-17 02:59:33 +0800

    fix

--- Chess/Viewer.cpp
+++ Chess/Viewer.cpp
@@ -188,11 +188,10 @@ void Viewer::reset() {
 PieceType Viewer::ask_promote() {
 	while (true)
 	{
-		cout << "Select character to promote to: (KQBNR)" << endl;
+		cout << "Select character to promote to: (QBNR)" << endl;
 		string promote;
 		cin >> promote;
 		if (promote.size() == 1) {
-			if (promote == "K")return PieceType::King;
 			if (promote == "Q")return PieceType::Queen;
 			if (promote == "B")return PieceType::Bishop;
 			if (promote == "N")return PieceType::Knight;
